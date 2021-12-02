cab2a0606c17e42590a7e609bfad8757a5114474
Sergo <matveevsergey1803@gmail.com>
2021-12-03 00:17:34 +0300

    Задание 3 выполнено

--- WinAsynchMethod/Form1.Designer.cs
+++ WinAsynchMethod/Form1.Designer.cs
@@ -45,6 +45,7 @@ namespace WinAsynchMethod
             this.btnRun.TabIndex = 0;
             this.btnRun.Text = "Сумма";
             this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
             // 
             // btnWork
             // 
@@ -54,6 +55,7 @@ namespace WinAsynchMethod
             this.btnWork.TabIndex = 1;
             this.btnWork.Text = "Работа";
             this.btnWork.UseVisualStyleBackColor = true;
+            this.btnWork.Click += new System.EventHandler(this.btnWork_Click);
             // 
             // ldlA
             // 
@@ -100,6 +102,7 @@ namespace WinAsynchMethod
             this.Controls.Add(this.btnRun);
             this.Name = "Form1";
             this.Text = "Асинхронный запуск";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
