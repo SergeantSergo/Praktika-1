97eebcbf0a3d7f04707e8e93a44e25943aa0a99e
Sergo <matveevsergey1803@gmail.com>
2021-12-02 23:33:49 +0300

    добавил делегат

--- WinAsynchDelegate/Form1.cs
+++ WinAsynchDelegate/Form1.cs
@@ -10,8 +10,28 @@ using System.Windows.Forms;
 
 namespace WinAsynchDelegate
 {
+    
     public partial class Form1 : Form
     {
+        private void TimeConsumingMethod(int seconds)
+        {
+            for (int j = 1; j <= seconds; j++)
+                System.Threading.Thread.Sleep(1000);
+        }
+        private delegate void TimeConsumingMethodDelegate(int seconds);
+        public delegate void SetProgressDelegate(int val);
+        public void SetProgress(int val)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                SetProgressDelegate del = new SetProgressDelegate(SetProgress);
+                this.Invoke(del, new object[] { val });
+            }
+            else
+            {
+                progressBar1.Value = val;
+            }
+        }
         public Form1()
         {
             InitializeComponent();
@@ -22,8 +42,10 @@ namespace WinAsynchDelegate
             if (!char.IsDigit(e.KeyChar))
             {
                 e.Handled = true;
-                MessageBox.Show("Поле должно содержать цифры")
+                MessageBox.Show("Поле должно содержать цифры");
             }
         }
+
     }
+
 }
