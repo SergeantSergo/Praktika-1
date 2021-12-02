530428bf7800ee6de1a12b6c8f41076437c5d5a1
Sergo <matveevsergey1803@gmail.com>
2021-12-02 23:52:24 +0300

    Добавил кнопки

--- WinAsynchMethod/Form1.Designer.cs
+++ WinAsynchMethod/Form1.Designer.cs
@@ -29,13 +29,90 @@ namespace WinAsynchMethod
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.ldlA = new System.Windows.Forms.Label();
+            this.IdIB = new System.Windows.Forms.Label();
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(16, 64);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Сумма";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // btnWork
+            // 
+            this.btnWork.Location = new System.Drawing.Point(120, 128);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(75, 23);
+            this.btnWork.TabIndex = 1;
+            this.btnWork.Text = "Работа";
+            this.btnWork.UseVisualStyleBackColor = true;
+            // 
+            // ldlA
+            // 
+            this.ldlA.AutoSize = true;
+            this.ldlA.Location = new System.Drawing.Point(8, 24);
+            this.ldlA.Name = "ldlA";
+            this.ldlA.Size = new System.Drawing.Size(65, 13);
+            this.ldlA.TabIndex = 2;
+            this.ldlA.Text = "Значение А";
+            // 
+            // IdIB
+            // 
+            this.IdIB.AutoSize = true;
+            this.IdIB.Location = new System.Drawing.Point(216, 24);
+            this.IdIB.Name = "IdIB";
+            this.IdIB.Size = new System.Drawing.Size(65, 13);
+            this.IdIB.TabIndex = 3;
+            this.IdIB.Text = "Значение B";
+            // 
+            // txbB
+            // 
+            this.txbB.Location = new System.Drawing.Point(296, 24);
+            this.txbB.Name = "txbB";
+            this.txbB.Size = new System.Drawing.Size(100, 20);
+            this.txbB.TabIndex = 4;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(88, 24);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(409, 161);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.IdIB);
+            this.Controls.Add(this.ldlA);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Асинхронный запуск";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnWork;
+        private System.Windows.Forms.Label ldlA;
+        private System.Windows.Forms.Label IdIB;
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.TextBox txtA;
     }
 }
 
