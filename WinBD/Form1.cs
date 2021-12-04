120b95cddc0fc74167b46a761ecf57825d3f4e2c
Sergo <matveevsergey1803@gmail.com>
2021-12-04 13:27:03 +0300

    выполняю задание 4_1

--- WinBD/Form1.cs
+++ WinBD/Form1.cs
@@ -16,5 +16,10 @@ namespace WinBD
         {
             InitializeComponent();
         }
+        //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
